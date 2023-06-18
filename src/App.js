1bb3579a46687b6b524a03119d60035afc7d3d72
Jakub Myrta <mkuba2006@gmail.com>
2023-06-18 15:39:43 +0200

    18.06

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import AddUser from './components/AddUser/AddUser'
-
+import UserList from './components/UserList/UserList';
+import React, { useState } from 'react';
 function App() {
+  const [list, uplist] = useState([]);
+
+  const update = (UName, UAge) =>{
+    uplist((pval)=>{
+      return[...pval,{name: UName, age: UAge, id: Math.random().toString()} ]
+    });
+    console.log(list);
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onUpdate={update}/>
+      <UserList  users={list}/>
     </div>
   );
 }
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -9,21 +9,42 @@ const basic_val = {
 
 
 function AddUser(props) {
-  const [val, setVal]= useState(basic_val)
+  const [Name, setName]= useState('');
+  const [Age, setAge]= useState('');
 
-
-  const change = (input, value) => {
-    setVal((pval)=>{
-      return{
-        ...pval,
-        [input]: +value
-      }
-    })
-    console.log(val);
-  }
   
+  // const [val, setVal]= useState(basic_val)
+
+
+  // const change = (input, value) => {
+  //   setVal((pval)=>{
+  //     return{
+  //       ...pval,
+  //       [input]: +value
+  //     }
+  //   })
+  //   console.log(val);
+  // }
+
+
+  const changeName = (e)=>{
+    setName(e.target.value);
+  }
+  const changeAge = (e)=>{
+    setAge(e.target.value);
+  }
+
   const submit = (e)=>{
     e.preventDefault();
+    if(Name.trim().length === 0 || Age.trim().length === 0){
+      return;
+    }
+    if(+Age < 1){
+      return;
+    }
+    props.onUpdate(Name,Age);
+    setName('');
+    setAge('');
   }
 
 
@@ -31,9 +52,11 @@ function AddUser(props) {
     <Card className={classes.input}>
       <form onSubmit={submit}>
         <label htmlFor='username' className={classes['label']}>Username</label>
-        <input onChange={(e)=> change('name', e.target.value)} type='text' id='name' value={val['name']}/>
+        {/* <input onChange={(e)=> change('name', e.target.value)} type='text' id='name' value={val['name']}/> */}
+        <input onChange={changeName} type='text' id='name'/>
         <label htmlFor='year'>Age (years)</label>
-        <input onChange={(e)=> change('year', e.target.value)} type='number' id='year' value={val['year']}/>
+        {/* <input onChange={(e)=> change('year', e.target.value)} type='number' id='year' value={val['year']}/> */}
+        <input onChange={changeAge}  type='number' id='year'/>
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
